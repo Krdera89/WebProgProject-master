fe69b81e937f6c1b439bbbf97197abfc30b0853c
Kayla Osterfeld <kkmosterfeld@gmail.com>
2022-02-24 02:31:23 -0500

    fixed some permissions and fixed nav bar

--- Pages/PersonPages/Edit.cshtml.cs
+++ Pages/PersonPages/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using WebProgProject.Models;
 
 namespace WebProgProject.Pages.PersonPages
 {
+    [Authorize]
     public class EditModel : PageModel
     {
         private readonly WebProgProject.Data.ApplicationDbContext _context;
