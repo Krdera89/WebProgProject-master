ed8006a55c28f8ab07574c4fa2025a5dc498a68d
Kayla Osterfeld <kmoste65@thomasmore.edu>
2022-04-19 21:47:49 -0400

    got the views or soemthing like that figured out to that i can edit things in the login part

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebProgProject.Data;
+
+[assembly: HostingStartup(typeof(WebProgProject.Areas.Identity.IdentityHostingStartup))]
+namespace WebProgProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
